2deea9e15928b9644380b189a9df701b3de88108
Jonas Nilsberg Refsnæs <jonas.nilsberg.refsnes@intility.no>
2023-11-28 17:36:17 +0100

    WIP

--- resources/views/flight.blade.php
+++ resources/views/flight.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout wire:poll>
     <div class="max-w-6xl m-auto">
         <h1 class="text-xl text-blue-900 my-2">Show flights:</h1>
-        <div class="grid grid-cols-4 gap-2">
+        <div class="grid md:grid-cols-2 lg:grid-cols-4 gap-2">
             @foreach($flights as $flight)
                 <div class="p-3 bg-amber-400" wire:key="{{$flight->id}}" wire:poll>
                     <span class="text-white" wire:poll>Name: {{$flight->name}}</span>
